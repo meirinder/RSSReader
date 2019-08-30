cc0d64bb9492f9b7b4cdf0a04abc7f4533d728e7
Savely Kulizhnikov <savely.kulizhnikov@sibers.com>
2019-08-30 16:45:01 +0700

    Initial Commit

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Savely on 30/08/2019.
+//  Copyright © 2019 Test App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
